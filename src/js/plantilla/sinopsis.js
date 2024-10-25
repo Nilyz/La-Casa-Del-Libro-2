cf49ee2e8eddf1448cc37a061e03992b395c5ca2
kiwan33 <rumansillar@gmail.com>
2024-10-25 21:40:38 +0200

    Actualizar estilos y comportamiento de la plantilla del libro

--- src/js/plantilla/sinopsis.js
+++ src/js/plantilla/sinopsis.js
@@ -1,29 +1,36 @@
-const sinopsisText = document.getElementById("sinopsis-text");
-const btnVermas = document.getElementById("btnVerMas");
+function initVerMasButton() {
+    const sinopsisText = document.getElementById("sinopsis-text");
+    const btnVermas = document.getElementById("btnVerMas");
 
-function updateVerMasButton() {
-    const totalHeight = sinopsisText.scrollHeight; // Altura total del texto
-    const containerHeight = sinopsisText.clientHeight; // Altura visible del contenedor
-
-    if (totalHeight > containerHeight) {
-        btnVermas.style.display = "block"; // Muestra el botón si el texto no cabe
-    } else {
-        btnVermas.style.display = "none"; // Oculta el botón si todo el texto cabe
+    if (!sinopsisText || !btnVermas) {
+        return; // Salir si los elementos no existen en el DOM
     }
+
+    function updateVerMasButton() {
+        const totalHeight = sinopsisText.scrollHeight; // Altura total del texto
+        const containerHeight = sinopsisText.clientHeight; // Altura visible del contenedor
+
+        if (totalHeight > containerHeight) {
+            btnVermas.style.display = "block"; // Muestra el botón si el texto no cabe
+        } else {
+            btnVermas.style.display = "none"; // Oculta el botón si todo el texto cabe
+        }
+    }
+
+    btnVermas.addEventListener("click", function () {
+        console.log("click");
+        if (sinopsisText.classList.contains("expanded")) {
+            sinopsisText.classList.remove("expanded");
+            btnVermas.textContent = "Ver más";
+        } else {
+            sinopsisText.classList.add("expanded");
+            btnVermas.textContent = "Ver menos";
+        }
+    });
+
+    // Llama a la función al cargar la página
+    updateVerMasButton();
+
+    // Llama a la función cuando se cambia el tamaño de la ventana
+    window.addEventListener("resize", updateVerMasButton);
 }
-
-btnVermas.addEventListener("click", function () {
-    if (sinopsisText.classList.contains("expanded")) {
-        sinopsisText.classList.remove("expanded");
-        btnVermas.textContent = "Ver más";
-    } else {
-        sinopsisText.classList.add("expanded");
-        btnVermas.textContent = "Ver menos";
-    }
-});
-
-// Llama a la función al cargar la página
-window.onload = updateVerMasButton;
-
-// Llama a la función cuando se cambia el tamaño de la ventana
-window.addEventListener("resize", updateVerMasButton);
