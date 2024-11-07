05ce79a933d7790408b7679a8d1d7387d55c55d2
kiwan33 <rumansillar@gmail.com>
2024-11-07 13:58:30 +0100

    Agregar icono de campana y aplicar estilos a eventos en la página de blog

--- src/js/blog.js
+++ src/js/blog.js
@@ -4,3 +4,31 @@ let actualidad = document.getElementById('actualidad');
 btnMasInformacion.addEventListener('click', () => {
     actualidad.scrollIntoView({ behavior: "smooth" });
 });
+
+function aplicarEstilos() {
+    const eventos = document.querySelectorAll('.evento');
+    const screenWidth = window.innerWidth;
+
+    // Limpiar todos los bordes antes de aplicar los nuevos
+    eventos.forEach(evento => {
+        evento.style.borderBottom = 'none';
+    });
+
+    // Si el ancho de la pantalla es mayor o igual a 992px, aplica borde a los 2 primeros eventos
+    if (screenWidth >= 992) {
+        for (let i = 0; i < eventos.length - 2; i++) {
+            eventos[i].style.borderBottom = '2px solid black';
+        }
+    }else{
+        for (let i = 0; i < eventos.length - 1; i++) {
+            eventos[i].style.borderBottom = '2px solid black';
+        }
+    }
+}
+
+// Aplica los estilos inicialmente
+aplicarEstilos();
+
+// Aplica los estilos cuando cambie el tamaño de la ventana
+window.addEventListener('resize', aplicarEstilos);
+
