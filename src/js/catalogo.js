5d4d92adf892ca24d7f97259fb411f50ebbe4a9e
Javier Sabadell Montero <sabadellmonterojavier@gmail.com>
2024-11-07 13:52:32 +0100

    Agregar funcionalidad de carrusel para libros, ajustar estilos y corregir rutas de scripts

--- src/js/catalogo.js
+++ src/js/catalogo.js
@@ -1,10 +1,14 @@
 function generateTrendingBooks() {
-    const bookAmount = 8;
-    for (let i = 0; i < bookAmount; i++) {
-        const book = document.createElement("div");
-        book.classList.add("book");
-        book.innerHTML = `
-            <img src="src/imagenes/libros/la-isla-de-la-mujer-dormida.webp" alt="libro" class="book__img" />
+const bookAmount = 5;
+const leftArrow = document.getElementById("arrow-right");
+const rightArrow = document.getElementById("arrow-right");
+const slider = document.getElementById("ficcion-books");
+for (let i = 0; i < bookAmount; i++) {
+    const book = document.createElement("div");
+    book.classList.add("book");
+    book.innerHTML = `
+            
+            <img src="../imagenes/libros/anatomia-del-mal.webp" alt="libro" class="book__img" />
             <div class="book__infoCont">
                 <p class="book__title">La isla de la mujer dormida</p>
                 <hr />
@@ -12,7 +16,21 @@ function generateTrendingBooks() {
                 <p class="book__price">12,50$</p>
             </div>
         `;
-        slider.appendChild(book);
+    slider.appendChild(book);
     }
 }
+function moveSlider(direction, section) {
+    if (section === "catalogo") {
+        if (direction === "right") {
+            if (currentOffset === 0) {
+            console.log("No hay más libros");
+            } else {
+                console.log("Mover a la derecha");
+            }
+        }
+    }
+}
+
+rightArrow.addEventListener("click", () => moveSlider("right", "catalogo"));
+leftArrow.addEventListener("click", () => moveSlider("left", "catalogo"));
 generateTrendingBooks();
