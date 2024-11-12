5faa374d6deed0fc0516cc7fe3b93a18f0c34ca0
Javier Sabadell Montero <sabadellmonterojavier@gmail.com>
2024-11-12 10:15:45 +0100

    Refactor catálogo: actualizar estilos, agregar secciones para diferentes géneros de ficción y optimizar la generación de libros

--- src/js/catalogo.js
+++ src/js/catalogo.js
@@ -1,6 +1,6 @@
-function generateTrendingBooks() {
+function generateFiccionContemporaneaBooks() {
     const bookAmount = 5;
-    const container = document.getElementById("ficcion-books"); 
+    const container = document.getElementById("ficcion-books-Contemporanea"); 
 
     for (let i = 0; i < bookAmount; i++) {
         const book = document.createElement("div");
@@ -18,4 +18,47 @@ function generateTrendingBooks() {
     }
 }
 
-generateTrendingBooks();
+function generateFiccionClasicaBooks() {
+    const bookAmount = 5;
+    const container = document.getElementById("ficcion-books-Clasica");
+
+    for (let i = 0; i < bookAmount; i++) {
+        const book = document.createElement("div");
+        book.classList.add("book");
+        book.innerHTML = `
+            <img src="../imagenes/libros/anatomia-del-mal.webp" alt="libro" class="book__img" />
+            <div class="book__infoCont">
+                <p class="book__title">La isla de la mujer dormida</p>
+                <hr />
+                <p class="book__autor">Autor del libro</p>
+                <p class="book__price">12,50$</p>
+            </div>
+        `;
+        container.appendChild(book); 
+    }
+}
+
+function generateFiccionTerrorBooks() {
+    const bookAmount = 5;
+    const container = document.getElementById("ficcion-books-Terror");
+
+    for (let i = 0; i < bookAmount; i++) {
+        const book = document.createElement("div");
+        book.classList.add("book");
+        book.innerHTML = `
+            <img src="../imagenes/libros/anatomia-del-mal.webp" alt="libro" class="book__img" />
+            <div class="book__infoCont">
+                <p class="book__title">La isla de la mujer dormida</p>
+                <hr />
+                <p class="book__autor">Autor del libro</p>
+                <p class="book__price">12,50$</p>
+            </div>
+        `;
+        container.appendChild(book); 
+    }
+}
+
+// Llamadas a las funciones
+generateFiccionContemporaneaBooks();
+generateFiccionClasicaBooks();
+generateFiccionTerrorBooks();
