275386f00e45664c3bc8b0c724e535efaf4f4d50
Javier Sabadell Montero <sabadellmonterojavier@gmail.com>
2024-11-13 10:19:53 +0100

    Actualizar generación de libros en el catálogo: integrar carga de datos desde JSON y mejorar la creación de elementos dinámicos

--- src/js/catalogo.js
+++ src/js/catalogo.js
@@ -1,63 +1,75 @@
-function generateFiccionContemporaneaBooks() {
+fetch("../../../pruebas/libros.json")
+    .then((response) => {
+        if (!response.ok)
+            throw new Error("No se pudo cargar el archivo review.json");
+        return response.json();
+    })
+    .then((data) => {
+        console.log("Datos recibidos:", data);
+        generateFiccionContemporaneaBooks(data);
+        generateFiccionClasicaBooks(data);
+        generateFiccionTerrorBooks(data);
+    });
+
+
+function generateFiccionContemporaneaBooks(data) {
     const bookAmount = 5;
     const container = document.getElementById("ficcion-books-Contemporanea"); 
-
     for (let i = 0; i < bookAmount; i++) {
+        const idLibro = i;
         const book = document.createElement("div");
         book.classList.add("book");
         book.innerHTML = `
-            <img src="../imagenes/libros/anatomia-del-mal.webp" alt="libro" class="book__img" />
+            <img src="../imagenes/libros/${data[idLibro].slug}webp" alt="${data[idLibro].slug}" class="book__img" />
             <div class="book__infoCont">
-                <p class="book__title">La isla de la mujer dormida</p>
+                <p class="book__title">${data[idLibro].nombre}</p>
                 <hr />
-                <p class="book__autor">Autor del libro</p>
-                <p class="book__price">12,50$</p>
+                <p class="book__autor">${data[idLibro].autor}</p>
+                <p class="book__price">${data[idLibro].precio}€</p>
             </div>
         `;
         container.appendChild(book); 
     }
 }
-
-function generateFiccionClasicaBooks() {
+function generateFiccionClasicaBooks(data) {
     const bookAmount = 5;
     const container = document.getElementById("ficcion-books-Clasica");
-
-    for (let i = 0; i < bookAmount; i++) {
+    for (let i = 0; i<bookAmount; i++){
+        const idLibro = i+5;
         const book = document.createElement("div");
         book.classList.add("book");
         book.innerHTML = `
-            <img src="../imagenes/libros/anatomia-del-mal.webp" alt="libro" class="book__img" />
+            <img src="../imagenes/libros/${data[idLibro].slug}webp" alt="${data[idLibro].slug}" class="book__img" />
             <div class="book__infoCont">
-                <p class="book__title">La isla de la mujer dormida</p>
+                <p class="book__title">${data[idLibro].nombre}</p>
                 <hr />
-                <p class="book__autor">Autor del libro</p>
-                <p class="book__price">12,50$</p>
+                <p class="book__autor">${data[idLibro].autor}</p>
+                <p class="book__price">${data[idLibro].precio}€</p>
             </div>
         `;
-        container.appendChild(book); 
+        container.appendChild(book);
     }
 }
-
-function generateFiccionTerrorBooks() {
+function generateFiccionTerrorBooks(data){
     const bookAmount = 5;
     const container = document.getElementById("ficcion-books-Terror");
-
-    for (let i = 0; i < bookAmount; i++) {
+    for(let i = 0; i<bookAmount; i++){
+        const idLibro = i+10;
+        const bookData = data.find((book) => book.id === idLibro);
         const book = document.createElement("div");
         book.classList.add("book");
         book.innerHTML = `
-            <img src="../imagenes/libros/anatomia-del-mal.webp" alt="libro" class="book__img" />
+            <img src="../imagenes/libros/${bookData.slug}.webp" alt="${bookData.slug}" class="book__img" />
             <div class="book__infoCont">
-                <p class="book__title">La isla de la mujer dormida</p>
+                <p class="book__title">${bookData.nombre}</p>
                 <hr />
-                <p class="book__autor">Autor del libro</p>
-                <p class="book__price">12,50$</p>
+                <p class="book__autor">${bookData.autor}</p>
+                <p class="book__price">${bookData.precio}€</p> 
             </div>
         `;
-        container.appendChild(book); 
+        container.appendChild(book);
     }
 }
-
 // Llamadas a las funciones
 generateFiccionContemporaneaBooks();
 generateFiccionClasicaBooks();
