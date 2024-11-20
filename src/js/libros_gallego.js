5fcc700a5ec1f1a4e4a24aa9d0eafd29ab309c30
Javier Sabadell Montero <sabadellmonterojavier@gmail.com>
2024-11-20 09:24:36 +0100

    Agregar scripts para generar libros juveniles, libros de texto y libros en gallego desde un archivo JSON

---
+++ src/js/libros_gallego.js
@@ -0,0 +1,78 @@
+fetch("../../../pruebas/libros.json")
+    .then((response) => {
+        if (!response.ok)
+            throw new Error("No se pudo cargar el archivo review.json");
+        return response.json();
+    })
+    .then((data) => {
+        console.log("Datos recibidos:", data);
+        generateFiccionContemporaneaBooks(data);
+        generateFiccionClasicaBooks(data);
+        generateFiccionTerrorBooks(data);
+    });
+
+
+function generateFiccionContemporaneaBooks(data) {
+    const bookAmount = 5;
+    const container = document.getElementById("books-Ciencias"); 
+    for (let i = 0; i < bookAmount; i++) {
+        const idLibro = i+76;
+        const bookData = data.find((book) => book.id === idLibro);
+        const book = document.createElement("div");
+        book.classList.add("book");
+        book.innerHTML = `
+            <img src="../imagenes/libros/${bookData.slug}.webp" alt="${bookData.slug}" class="book__img" />
+            <div class="book__infoCont">
+                <p class="book__title">${bookData.nombre}</p>
+                <hr />
+                <p class="book__autor">${bookData.autor}</p>
+                <p class="book__price">${bookData.precio}€</p>
+            </div>
+        `;
+        container.appendChild(book); 
+    }
+}
+function generateFiccionClasicaBooks(data) {
+    const bookAmount = 5;
+    const container = document.getElementById("books-Biografias");
+    for (let i = 0; i<bookAmount; i++){
+        const idLibro = i+81;
+        const bookData = data.find((book) => book.id === idLibro);
+        const book = document.createElement("div");
+        book.classList.add("book");
+        book.innerHTML = `
+            <img src="../imagenes/libros/${bookData.slug}.webp" alt="${bookData.slug}" class="book__img" />
+            <div class="book__infoCont">
+                <p class="book__title">${bookData.nombre}</p>
+                <hr />
+                <p class="book__autor">${bookData.autor}</p>
+                <p class="book__price">${bookData.precio}€</p>
+            </div>
+        `;
+        container.appendChild(book);
+    }
+}
+function generateFiccionTerrorBooks(data){
+    const bookAmount = 5;
+    const container = document.getElementById("books-Criminologia");
+    for(let i = 0; i<bookAmount; i++){
+        const idLibro = i+86;
+        const bookData = data.find((book) => book.id === idLibro);
+        const book = document.createElement("div");
+        book.classList.add("book");
+        book.innerHTML = `
+            <img src="../imagenes/libros/${bookData.slug}.webp" alt="${bookData.slug}" class="book__img" />
+            <div class="book__infoCont">
+                <p class="book__title">${bookData.nombre}</p>
+                <hr />
+                <p class="book__autor">${bookData.autor}</p>
+                <p class="book__price">${bookData.precio}€</p> 
+            </div>
+        `;
+        container.appendChild(book);
+    }
+}
+// Llamadas a las funciones
+generateFiccionContemporaneaBooks();
+generateFiccionClasicaBooks();
+generateFiccionTerrorBooks();
